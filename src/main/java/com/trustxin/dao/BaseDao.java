16ab31677be60cef18decefe958c07820c7e0989
Chris-lyj <china.jerry.lu@gmail.com>
2019-02-13 13:56:11 +0800

    核心bean代码

--- src/main/java/com/trustxin/dao/BaseDao.java
+++ src/main/java/com/trustxin/dao/BaseDao.java
@@ -7,11 +7,9 @@ import com.alibaba.druid.sql.ast.statement.*;
 import com.alibaba.druid.sql.visitor.SQLASTOutputVisitor;
 import com.alibaba.druid.util.JdbcUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Repository;
 
 import java.lang.reflect.Field;
 import java.util.*;
@@ -880,7 +878,7 @@ public class BaseDao {
     }
 
     /**
-     * 结果集转化驼峰式
+     * 结果集属性名转化驼峰式
      * @param results
      * @return
      */
